7fc4af94931eba43df76b6991bd7b774a0497250
dzikra24ti <dzikra24ti@mahasiswa.pcr.ac.id>
2026-01-01 10:00:42 +0700

    tes

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
         'address',
         'is_active',
     ];
-
+        
     /**
      * The attributes that should be hidden for serialization.
      *
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
 use App\Http\Controllers\DashboardController;
-
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->name('dashboard');
 
 use App\Http\Controllers\TransaksiController;
 Route::resource('transaksi', TransaksiController::class);
